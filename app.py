6cf964af5b74c3a3ca4307771230a261bacbc7a3
Vasan <keerthivasan83000@gmail.com>
2026-09-25 14:23:01 +0530

    ops: disable development debug server

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from aegis_core import initialize_sentinel_platform
 
-
 app = initialize_sentinel_platform()
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
